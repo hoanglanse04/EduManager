cb56498b157a8282d3624226ad373c11b7ba4888
hoanglanse04 <hoanglando15@gmail.com>
2024-09-19 22:01:56 +0700

    feat: add EduManager

---
+++ BTL/ConnectString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL
+{
+    public class ConnectString
+    {
+        public static string GetConnectionString()
+            {
+                return @"Data Source=HOANGLANNN\SQLEXPRESS;Initial Catalog=QuanLyDiemSV;Integrated Security=True";
+            }
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EduManager
+DỰ ÁN QUẢN LÝ CÁC THÔNG TIN VỀ SINH VIÊN, GIẢNG VIÊN, MÔN HỌC, LỚP HÀNH CHÍNH, ĐIỂM.... CỦA TRƯỜNG ĐẠI HỌC
